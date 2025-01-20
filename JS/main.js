8b4915c7a93d67299700f0f3a8042e0af0a15431
YutoImata <SBA24J10001@edu.sba.ac.jp>
2025-01-20 16:16:59 +0900

    Add next tasks

--- JS/main.js
+++ JS/main.js
@@ -1,6 +1,6 @@
 // グローバル変数からクラスを使用
 const deck = new Deck();
-deck.shuffle(); /* 'draw-card'に入れたら毎度シャッフルされ同じカードが出てきてしまうためここに書く */
+deck.shuffle(); /* 'draw-card'の関数内に入れたら毎度シャッフルされ同じカードが出てきてしまうためここに書く */
 
 document.getElementById('draw-card').addEventListener('click', () => {
 const drawCard = deck.draw();
