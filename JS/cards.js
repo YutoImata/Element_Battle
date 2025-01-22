a4f574c1401d504f8a623e4b42deef0c93907da6
YutoImata <SBA24J10001@edu.sba.ac.jp>
2025-01-22 17:57:58 +0900

    feat: add card to playarea

--- JS/cards.js
+++ JS/cards.js
@@ -70,27 +70,31 @@ function createCardElement(card) {
     return cardElement;
 }
 
-
+/* プレイヤー手札からカードを選択する関数 */
+function selectCardFromPlayerHand(cardElement) {
+    const selectedCard = document.querySelector('.selected-card');
+    if (selectedCard) {
+        selectedCard.classList.remove('selected-card');
+    }
+    cardElement.classList.add('selected-card');
+}
 
 /* プレイヤーに手札を追加する関数 */
 function addCardToPlayerHand(card) {
     const playerHand = document.querySelector('#player-hand #cards'); /* #player-hand 内の #cardsを取得する */
-    console.log('Player hand:', card.toString()); /* デバック */
+    // console.log('Player hand:', card.toString()); /* デバック */
     const cardElement = createCardElement(card);
+    cardElement.addEventListener('click', () => selectCardFromPlayerHand(cardElement));
     playerHand.appendChild(cardElement);
 }
 
 /* 相手のに手札を追加する関数 */
 function addCardToOpponentHand(card) {
     const opponentHand = document.querySelector('#opponent-hand #cards');
-    console.log('Opponent hand:', card.toString()); /* デバック */
+    // console.log('Opponent hand:', card.toString()); /* デバック */
     const cardElement = createCardElement(card);
     opponentHand.appendChild(cardElement);
 }
-// /* デバック用に敵の手札が見えるようにする */
-// document.getElementById('debug-toggle').addEventListener('click', () => {
-//     document.body.classList.toggle('debug');
-// })
 
 /* 各プレイヤーにカードを５枚ずつ最初に配る */
 function dealInitialCards () {
@@ -99,3 +103,31 @@ function dealInitialCards () {
         addCardToOpponentHand(deck.draw());
     }
 }
+
+/* プレイエリアにカードを出せるようにする関数 */
+function playCard() {
+    const selectedCard = document.querySelector('.selected-card');
+    if (!selectedCard) {
+        alert('カードを選択してください！');
+        return;
+    }
+
+    const playArea = document.querySelector('#play-area #played-cards');
+    
+    /* カードをプレイエリアに移動させる */
+    selectedCard.style.position = 'absolute';
+    selectedCard.style.top = `${playArea.offsetTop}px`
+    selectedCard.style.left = `${playArea.offsetLeft}px`
+
+    /* 少し経ったらプレイエリアにカードを移動させる */
+    setTimeout(() => {
+        playArea.appendChild(selectedCard);
+        selectedCard.classList.remove('selected-card');
+        selectedCard.style.position = '';
+        selectedCard.style.top = '';
+        selectedCard.style.left = '';
+    }, 500); /* アニメーションの時間と同じに設定する */
+}
+
+/* カードを出すボタンのイベントリスナーを追加する */
+document.getElementById('play-card').addEventListener('click', playCard);
