5f6dbc9495954975cc0fe8bc0136967c15f10923
YutoImata <SBA24J10001@edu.sba.ac.jp>
2025-01-23 13:56:28 +0900

    feat: add way to win

--- JS/rule.js
+++ JS/rule.js
@@ -30,20 +30,25 @@
             }
         }else if (card.suit === '♠') {
             if (isPlayer) {
-                opponentElementPoints += Math.max(0, opponentElementPoints - 1);
+                opponentElementPoints += Math.max(0, opponentElementPoints - 2);
             }else{
-                playerElementPoints += Math.max(0, playerElementPoints - 1);
+                playerElementPoints += Math.max(0, playerElementPoints - 2);
             }
-
-            /* 
-            Math.max(0, opponentElementPoints - 1);
-            これは敵のエレメントポイントが負の数に行かないようにしている 
-            */
+        } else if (card.suit === 'Joker') { /* デバック用です/なぜかできない */
+            if (isPlayer) {
+                opponentElementPoints += 10;
+            } else {
+                playerElementPoints += 10;
+            }
         }
         console.log('自分：',playerElementPoints);
         console.log('敵：',opponentElementPoints);
 
-        updateElementPointsDisplay();
+         /* 
+            Math.max(0, opponentElementPoints - 1);
+            これは敵のエレメントポイントが負の数に行かないようにしている 
+            */
+
     }
 
     /* エレメントポイントを表示する関数 */
@@ -51,3 +56,29 @@
         document.getElementById('player-points').textContent = playerElementPoints;
         document.getElementById('opponent-points').textContent = opponentElementPoints;
     }
+
+    /* 勝利かどうかを判定する関数 */
+    function checkForWin() {
+        if (playerElementPoints >= 10) {
+            showVictoryMessage();
+            // resetGame();
+        }else if (opponentElementPoints >= 10) {
+            showLoseMessage(); 
+            // resetGame();
+        }
+    }
+
+    /* 勝利メッセージを表示する関数 */
+    function showVictoryMessage() {
+    const victoryMessage = document.getElementById('victory-message');
+    victoryMessage.style.display = 'block';
+    }
+
+    function showLoseMessage() {
+    const loseMessage = document.getElementById('lose-message');
+    loseMessage.style.display = 'block';
+    }
+
+
+    
+
