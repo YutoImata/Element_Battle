ddbe7dadecad72f5fcdd0fe1416bafda2e7092a6
YutoImata <SBA24J10001@edu.sba.ac.jp>
2025-01-23 13:41:15 +0900

    feat: add update function

--- JS/main.js
+++ JS/main.js
@@ -8,6 +8,30 @@
 const deck = new Deck();
 deck.shuffle(); /* 'draw-card'の関数内に入れたら毎度シャッフルされ同じカードが出てきてしまうためここに書く */
 
+
+function update() {
+    /* カードを引くボタンを押したらカードを引く */
+    document.getElementById('draw-card').addEventListener('click', () => {
+        const drawCard = deck.draw();
+        addCardToPlayerHand(drawCard);
+    });
+
+
+    /* エレメントポイントをHTML上に反映させる方法 */
+    document.getElementById('play-card').addEventListener('click', () => {
+        playCard();
+        const selectedCard = document.querySelector('.selected-card');
+        if (selectedCard) {
+            const card = new Card(selectedCard.textContent[0],selectedCard.textContent.slice(1));
+            applyCardEffect(card, true);
+
+            updateElementPointsDisplay(); /* エレメントポイントを更新 */
+            checkForWin(); /* 勝利しているかをチェック */
+        }
+    });
+}
+    
+
 /* スタートボタンが押されたらカードをお互いに５枚ずつ配布する */
 document.addEventListener('DOMContentLoaded', () => {
     /* カードを５枚配るのと、背景の透明化を解除する */
@@ -15,21 +39,6 @@ document.addEventListener('DOMContentLoaded', () => {
         document.getElementById('game-board').classList.remove('transparent-background');
         document.getElementById('start-game').style.display = 'none';
         dealInitialCards();
+        update();
     });
-
-    /* カードを引くボタンを押したらカードを引く */
-    document.getElementById('draw-card').addEventListener('click', () => {
-        const drawCard = deck.draw();
-        addCardToPlayerHand(drawCard);
-    });
-});
-
-/* エレメントポイントをHTML上に反映させる方法 */
-document.getElementById('play-card').addEventListener('click', () => {
-    playCard();
-    const selectedCard = document.querySelector('.selected-card');
-    if (selectedCard) {
-        const card = new Card(selectedCard.textContent[0],selectedCard.textContent.slice(1));
-        applyCardEffect(card, true);
-    }
 });
