9325dd5a50286525890dc310fd49de893ea4feaa
YutoImata <yuto.imata.wine@gmail.com>
2025-01-26 20:43:56 +0900

    feat: add ♠A option

--- JS/specialRules.js
+++ JS/specialRules.js
@@ -9,8 +9,12 @@ specialCard = false;
 
 /* カードを出したときのエレメントポイントの計算 */
 function applySpecialCardRules(card, isPlayer) {
-    if (card.rank === '7') {
-        playerElementPoints += 7;
-        specialCard = true;
+    if (card.suit === '♠' && card.rank === 'A') {
+        /* 自分と敵のポイントを入れ替える */
+        const tempOpponentElementPoints = opponentElementPoints;
+        opponentElementPoints = playerElementPoints;
+        playerElementPoints = tempOpponentElementPoints;
+        
+        specialCard = true; /* フラグをtrueにする */
     }
 }
