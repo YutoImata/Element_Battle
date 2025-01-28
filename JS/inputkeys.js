63681c1c3ace1b639256a20aec99bb5180d82ad7
YutoImata <yuto.imata.wine@gmail.com>
2025-01-28 23:39:14 +0900

    feat: add H key animation when user push it

--- JS/inputkeys.js
+++ JS/inputkeys.js
@@ -11,7 +11,12 @@ document.addEventListener('keydown', function(event) {
         if (overlay.classList.contains('show')) {
             overlay.classList.remove('show'); /* 画像を非表示にする */
         } else {
+            overlay.style.opacity = 0; /* 初期透明度を設定 */
             overlay.classList.add('show'); /* 画像を表示する */
+            setTimeout(() => {
+                overlay.style.transition = 'opacity 0.5s'; // フェードイン用のトランジション設定
+                overlay.style.opacity = 1; // 不透明にしてフェードイン開始
+            }, 100); // 0.1秒後にトランジションを適用
         }
     } else if (event.key === 'Escape') {
         overlay.classList.remove('show'); /* 画像を非表示にする */
