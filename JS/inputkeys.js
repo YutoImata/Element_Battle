b1edcb89c387954a24007c9b72e28beeff703648
YutoImata <yuto.imata.wine@gmail.com>
2025-01-30 14:56:56 +0900

    chore: delete comments

--- JS/inputkeys.js
+++ JS/inputkeys.js
@@ -14,8 +14,8 @@ document.addEventListener('keydown', function(event) {
             overlay.style.opacity = 0; /* 初期透明度を設定 */
             overlay.classList.add('show'); /* 画像を表示する */
             setTimeout(() => {
-                overlay.style.transition = 'opacity 0.5s'; // フェードイン用のトランジション設定
-                overlay.style.opacity = 1; // 不透明にしてフェードイン開始
+                overlay.style.transition = 'opacity 0.5s'; 
+                overlay.style.opacity = 1; 
             }, 100); // 0.1秒後にトランジションを適用
         }
     } else if (event.key === 'Escape') {
