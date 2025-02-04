a703d15583db5ee60b32bb0d8427a49239ed9cb5
YutoImata <yuto.imata.wine@gmail.com>
2025-02-04 19:35:47 +0900

    feat: show rules on title screen

--- JS/index.js
+++ JS/index.js
@@ -151,6 +151,36 @@ function generateCardsBackground() {
     backgroundDiv.style.zIndex = '-1'; 
 }
 
+/* 「H」キーを押したときに画像を表示 */
+document.addEventListener('DOMContentLoaded', function() {
+    const overlay = document.getElementById('overlay');
+    
+    function toggleOverlay() {
+        if (overlay.classList.contains('show')) {
+            overlay.classList.remove('show'); /* 画像を非表示にする */
+        } else {
+            overlay.style.opacity = 0; /* 初期透明度を設定 */
+            overlay.classList.add('show'); /* 画像を表示する */
+            setTimeout(() => {
+                overlay.style.transition = 'opacity 0.5s'; 
+                overlay.style.opacity = 1; 
+            }, 100); // 0.1秒後にトランジションを適用
+        }
+    }
+
+    /* Hキーで表示・非表示 */
+    document.addEventListener('keydown', function(event) {
+        if (event.key === 'H' || event.key === 'h') {
+            toggleOverlay();
+        } else if (event.key === 'Escape') {
+            overlay.classList.remove('show'); /* 画像を非表示にする */
+        }
+    });
+
+});
+
+
+
 /* タイトル画面からゲーム画面の移動 */
 document.getElementById('gama-page-button').addEventListener('click', function () {
     window.location.href = 'HTML/game.html';
