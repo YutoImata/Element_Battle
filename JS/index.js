a8af42242c729b32e00579e9c111bb57ee4a061b
YutoImata <yuto.imata.wine@gmail.com>
2025-02-08 16:09:22 +0900

    feat: disable various keys and actions（各種キーや動作の無効化）

--- JS/index.js
+++ JS/index.js
@@ -217,6 +217,30 @@ document.addEventListener('DOMContentLoaded', function() {
 
 });
 
+/* 各種キーや動作の無効化 */
+/* キーボードショートカットの無効化 */
+document.addEventListener("keydown", function (event) {
+    if (
+        event.key === "F12" || /* デベロッパーツール */
+        (event.ctrlKey && event.key === "+") || /* ズームイン */
+        (event.ctrlKey && event.key === "-") || /*  ズームアウト */
+        (event.ctrlKey && event.key === "0") ||  /* ズームリセット */
+        (event.ctrlKey && event.key === "u") || /*  ページソースの表示 (Ctrl + U) */
+        (event.ctrlKey && event.shiftKey && event.key === "I") || /*  デベロッパーツール (Ctrl + Shift + I) */
+        (event.ctrlKey && event.shiftKey && event.key === "J") /*  デベロッパーツール (Ctrl + Shift + J) */
+    ) {
+        event.preventDefault();
+        event.stopPropagation();
+        return false;
+    }
+});
+
+/*  右クリックメニューの無効化 */
+document.addEventListener("contextmenu", function (event) {
+    event.preventDefault();
+    return false;
+});
+
 // /* カスタマイズの矢印を押したら向きが変わる　*/
 // function toggleCheckboxContainer() {
 //     document.getElementById("toggle-container").addEventListener("click", function() {
